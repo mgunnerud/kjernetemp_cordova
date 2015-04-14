ce4c501101c7d67a7cd88f02c620df19c85fdfce
Martin <mgunnerud@gmail.com>
2015-04-14 23:08:21 +0200

    use cordova pageslider

---
+++ www/js/data.js
@@ -0,0 +1,99 @@
+var data = [
+            {
+            "name": "Svin",
+            "children": [
+                         {
+                         "name": "Kotelett",
+                         "children": [{ "name": "rare", "description": "x grader" }]
+                         },
+                         {
+                         "name": "Svinesteik",
+                         "children": [
+                                      {
+                                      "name": "rare",
+                                      "description": "50-55 grader"
+                                      },
+                                      {
+                                      "name": "medium",
+                                      "description": "57-60 grader"
+                                      },
+                                      {
+                                      "name": "well done",
+                                      "description": "61-67 grader"
+                                      }]
+                         }
+                         ]
+            },
+            {
+            "name": "Lam",
+            "children": [{ "name": "Lammesteik", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Lammekotelett", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 1",
+            "children": [{ "name": "Test 1-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 1-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 2",
+            "children": [{ "name": "Test 2-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 2-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 3",
+            "children": [{ "name": "Test 3-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 3-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 4",
+            "children": [{ "name": "Test 4-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 4-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 5",
+            "children": [{ "name": "Test 5-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 5-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 6",
+            "children": [{ "name": "Test 6-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 6-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 7",
+            "children": [{ "name": "Test 7-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 7-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 8",
+            "children": [{ "name": "Test 8-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 8-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 9",
+            "children": [{ "name": "Test 9-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 9-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 10",
+            "children": [{ "name": "Test 10-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 10-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 11",
+            "children": [{ "name": "Test 11-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 11-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 12",
+            "children": [{ "name": "Test 12-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 12-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 13",
+            "children": [{ "name": "Test 13-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 13-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 14",
+            "children": [{ "name": "Test 14-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 14-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 15",
+            "children": [{ "name": "Test 15-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 15-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test 16",
+            "children": [{ "name": "Test 16-1", "children": [{ "name": "rare", "description": "x grader" }] },{ "name": "Test 16-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            },
+            {
+            "name": "Test nederste trenode",
+            "children": [{ "name": "Test nederst-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test nederst-2", "children": [{ "name": "rare", "description": "x grader" }] }]
+            }
+            ];
--- www/js/kjernetemp.js
+++ www/js/kjernetemp.js
@@ -2,137 +2,33 @@
 * TODO:
 * - handle screen rotation
 * - add styling
+* - remove jQuery dependency
 */
-var screenWidth;
-var currentScreen = 0;
-var scrollSpeed = 200;
-var headerHistory = ["Velg kjøtt"];
-
-var data = [
-	{
-		"name": "Svin",
-		"children": [
-			{ 
-				"name": "Kotelett", 
-				"children": [{ "name": "rare", "description": "x grader" }]
-			}, 
-			{ 
-				"name": "Svinesteik", 
-				"children": [
-				{ 
-					"name": "rare", 
-					"description": "50-55 grader" 
-				}, 
-				{ 
-					"name": "medium",
-					"description": "57-60 grader" 
-				}, 
-				{ 
-					"name": "well done",
-					"description": "61-67 grader" 
-				}]
-			}
-		]
-	},
-	{
-		"name": "Lam",
-		"children": [{ "name": "Lammesteik", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Lammekotelett", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},
-	{
-		"name": "Test 1",
-		"children": [{ "name": "Test 1-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 1-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},
-	{
-		"name": "Test 2",
-		"children": [{ "name": "Test 2-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 2-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},
-	{
-		"name": "Test 3",
-		"children": [{ "name": "Test 3-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 3-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},
-	{
-		"name": "Test 4",
-		"children": [{ "name": "Test 4-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 4-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},
-	{
-		"name": "Test 5",
-		"children": [{ "name": "Test 5-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 5-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},
-	{
-		"name": "Test 6",
-		"children": [{ "name": "Test 6-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 6-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},
-	{
-		"name": "Test 7",
-		"children": [{ "name": "Test 7-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 7-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},
-	{
-		"name": "Test 8",
-		"children": [{ "name": "Test 8-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 8-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},		
-	{
-		"name": "Test 9",
-		"children": [{ "name": "Test 9-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 9-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},
-	{
-		"name": "Test 10",
-		"children": [{ "name": "Test 10-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 10-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},
-	{
-		"name": "Test 11",
-		"children": [{ "name": "Test 11-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 11-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},
-	{
-		"name": "Test 12",
-		"children": [{ "name": "Test 12-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 12-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},
-	{
-		"name": "Test 13",
-		"children": [{ "name": "Test 13-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 13-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},
-	{
-		"name": "Test 14",
-		"children": [{ "name": "Test 14-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 14-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	},
-    {
-        "name": "Test 15",
-        "children": [{ "name": "Test 15-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test 15-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-    },
-    {
-        "name": "Test 16",
-        "children": [{ "name": "Test 16-1", "children": [{ "name": "rare", "description": "x grader" }] },{ "name": "Test 16-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-    },
-	{
-		"name": "Test nederste trenode",
-		"children": [{ "name": "Test nederst-1", "children": [{ "name": "rare", "description": "x grader" }] }, { "name": "Test nederst-2", "children": [{ "name": "rare", "description": "x grader" }] }]
-	}
-];
+var defaultHeaderName = "Velg kjøtt";
+var slider;
 
 function Kjernetemp()
 {
 	var me = this;
-	var scrollPane = me.getScrollPane();
 	me.setupTabButtons();
 	
-	
 	var $firstView = document.createElement("div");
-	$firstView.classList.add("card");
-	$firstView.style.width = screenWidth + "px";
 	me.fillView($firstView, data);
 	
 	var $backButton = document.getElementById("backButton");
-	$backButton.style.display = "none";
+	me.setBackButtonVisibility(false);
 	var backButtonClickFn = function (event)
 	{
 		event.preventDefault();
-		me.scrollPrev();
-	};
+        var lastIndexOfBreak = window.location.hash.lastIndexOf("-");
+        window.location.hash = window.location.hash.substr(0, lastIndexOfBreak);
+        me.doScroll();
+    };
 	$backButton.addEventListener("click", backButtonClickFn, false);
-	//$backButton.addEventListener("touchend", backButtonClickFn, false);
-	me.setHeaderName();
+	me.setHeaderName(defaultHeaderName);
 	
-    scrollPane.style.width = screenWidth + "px";
-	scrollPane.appendChild($firstView);
+    window.location.hash = "0";
+    slider.slidePage($($firstView));
 };
 
 Kjernetemp.prototype.setupTabButtons = function()
@@ -153,7 +49,6 @@ Kjernetemp.prototype.setupTabButtons = function()
 		$contactButton.classList.remove("activeTabButton");
 	};
 	$temperatureButton.addEventListener("click", onTemperatureButtonClick, false);
-	//$temperatureButton.addEventListener("touchend", onTemperatureButtonClick, false);
 	
 	var onContactButtonClick = function(event)
 	{
@@ -164,7 +59,6 @@ Kjernetemp.prototype.setupTabButtons = function()
 		$contactButton.classList.add("activeTabButton");
 	};
 	$contactButton.addEventListener("click", onContactButtonClick, false);
-	//$contactButton.addEventListener("touchend", onContactButtonClick, false);
 };
 
 Kjernetemp.prototype.fillView = function(view, viewData)
@@ -190,19 +84,8 @@ Kjernetemp.prototype.fillView = function(view, viewData)
 		{
 			event.preventDefault();
 			var treePath = event.currentTarget.getAttribute("treePath");
-			var scrollPane = me.getScrollPane();
-			var $newCard = document.createElement("div");
-			me.fillView($newCard, viewData[treePath].children);
-			$newCard.style.width = screenWidth + "px";
-			$newCard.classList.add("card");
-			
-			headerHistory.push(viewData[treePath].name);
-			me.setHeaderName();
-			
-			scrollPane.appendChild($newCard);
-			scrollPane.style.width = scrollPane.clientWidth + screenWidth + "px";
-			
-			setTimeout(function() { me.scrollNext(); }, 10); // TODO: better fix for this!!!
+			window.location.hash = window.location.hash + "-" + treePath;
+            me.doScroll();
 		};
 		
 		listObject.setAttribute("treePath", i);
@@ -210,7 +93,6 @@ Kjernetemp.prototype.fillView = function(view, viewData)
 		{
             listObject.classList.add("nodeHasChildren");
 			listObject.addEventListener("click", listObjectClickFn, false);
-			//listObject.addEventListener("touchend", listObjectClickFn, false);
 			var listObjectArrow = document.createElement("div");
 			listObjectArrow.innerHTML = ">";
 			listObjectArrow.classList.add("listObjectArrow");
@@ -220,66 +102,51 @@ Kjernetemp.prototype.fillView = function(view, viewData)
 	} 
 };
 
-Kjernetemp.prototype.setHeaderName = function()
+Kjernetemp.prototype.setHeaderName = function(headerName)
 {
-	var headerNameEl = document.getElementById("headerName");
-	headerNameEl.innerHTML = headerHistory[headerHistory.length - 1];
-};
-
-Kjernetemp.prototype.getScrollPane = function()
-{
-	return document.getElementById("scrollpane");
-};
-
-Kjernetemp.prototype.scrollNext = function()
-{
-	var me = this;
-	currentScreen += 1;
-	document.getElementById("backButton").style.display = "";
-	me.doScroll();
-	setTimeout(function() {
-		me.getScrollPane().style.transition = "";
-	}, scrollSpeed);
-};
-
-Kjernetemp.prototype.scrollPrev = function()
-{
-	var me = this;
-	currentScreen -= 1;
-	if(currentScreen === 0)
-		document.getElementById("backButton").style.display = "none";
-	
-	me.doScroll();
-	headerHistory.pop();
-	me.setHeaderName();
-	setTimeout(function() {
-		me.getScrollPane().style.width = me.getScrollPane().clientWidth - screenWidth + "px";
-		me.getScrollPane().removeChild(me.getScrollPane().lastChild);
-		me.getScrollPane().style.transition = "";
-	}, scrollSpeed); // TODO: better fix for removing last child.
+	document.getElementById("headerName").innerHTML = headerName;
 };
 
 Kjernetemp.prototype.doScroll = function()
 {
-	var me = this;
-	me.getScrollPane().style.transition = scrollSpeed + "ms"; // enable animation of card movement.
-	me.getScrollPane().style.right = screenWidth * currentScreen + "px";
+    var me = this;
+    var pathToView = window.location.hash.split("-");
+    
+    var newViewData;
+    var headerName;
+    if(pathToView.length === 1)
+    {
+        newViewData = data;
+        headerName = defaultHeaderName;
+    }
+    else if(pathToView.length === 2)
+    {
+        newViewData = data[parseInt(pathToView[1])].children;
+        headerName = data[parseInt(pathToView[1])].name;
+    }
+    else if(pathToView.length === 3)
+    {
+        newViewData = data[parseInt(pathToView[1])].children[parseInt(pathToView[2])].children;
+        headerName = data[parseInt(pathToView[1])].children[parseInt(pathToView[2])].name;
+    }
+
+    me.setHeaderName(headerName);
+    me.setBackButtonVisibility(pathToView.length > 1);
+    
+    var $newCard = document.createElement("div");
+    me.fillView($newCard, newViewData);
+    slider.slidePage($($newCard));
 };
-/*
-// for testing. Replace by swipe motions
-document.addEventListener("keydown", function(e)
+
+Kjernetemp.prototype.setBackButtonVisibility = function(visible)
 {
-	e = e || window.event;
-	
-	if(e.keyCode === 37) //left
-		kjernetempGlobal.scrollPrev();
-}, false);
-*/
+    document.getElementById("backButton").style.display = visible ? "" : "none";
+}
 
 window.onload = function () 
 {
-	screenWidth = document.getElementById("temperatureTree").clientWidth;
+    slider = new PageSlider($(document.getElementById("scrollpane")));
 	kjernetempGlobal = new Kjernetemp();
    	var attachFastClick = Origami.fastclick;
-	attachFastClick(document.body);
+    attachFastClick(document.body);
 };
